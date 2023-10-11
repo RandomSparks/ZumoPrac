b48ac4ad3856558c1721c5d3b59d0a5bf20b5999
RandomSparks <109492406+RandomSparks@users.noreply.github.com>
2023-10-11 12:19:48 +1030

    initial follow line

--- ZumoPrac.ino
+++ ZumoPrac.ino
@@ -10,9 +10,9 @@
 // Note: make sure each encoding is unique
 
 #define STATE_FORWARD 0
-#define STATE_LEFT    1
-#define STATE_RIGHT   2
-#define STATE_HALT    3
+#define STATE_LEFT 1
+#define STATE_RIGHT 2
+#define STATE_HALT 3
 
 #define FINISH_MODE 0
 #define LINE_FOLLOW_MODE 1
@@ -29,7 +29,6 @@ int next_state = STATE_FORWARD;
 uint8_t mode[] = {LINE_FOLLOW_MODE, JUMP_GAP_MODE, FINISH_MODE};
 uint8_t current_mode = mode[0];
 
-
 void stateForward()
 {
   Serial.println("forward");
@@ -41,86 +40,94 @@ void stateLeft()
 {
   Serial.println("left");
   Serial0.println("left");
-  motors.setSpeeds(SPEED_HALT, SPEED_MAX);  
+  motors.setSpeeds(SPEED_HALT, SPEED_MAX);
 }
 
 void stateRight()
 {
   Serial.println("right");
   Serial0.println("right");
-  motors.setSpeeds(SPEED_MAX, SPEED_HALT);  
+  motors.setSpeeds(SPEED_MAX, SPEED_HALT);
 }
 
 void stateHalt()
 {
   Serial.println("halt");
   Serial0.println("halt");
-  motors.setSpeeds(SPEED_HALT,SPEED_HALT);
+  motors.setSpeeds(SPEED_HALT, SPEED_HALT);
 }
 
 /* -------END STATE IMPLEMENTATION---------- */
 
 /* -----------STATE CHOICE---------- */
 
-void selectState() {
- 
+void selectState()
+{
+
   position = reflectanceSensors.readLine(sensors);
 
   Serial0.println("pos: " + position);
   Serial.println("pos: " + position);
+}
 
-  if(state == STATE_FORWARD)
+void line_follow()
+{
+
+  while (!no_line)
   {
-    
-    if(position < 1000) // Put your condition here
+    if (state == STATE_FORWARD)
     {
-      // Write your desired state here
-      next_state = STATE_LEFT;
+      bool no_line = false;
+      if (position < 1000) // Put your condition here
+      {
+        // Write your desired state here
+        next_state = STATE_LEFT;
+      }
+      else if (position > 4000) // Put your condition here
+      {
+        next_state = STATE_RIGHT;
+      }
+      else if ((sensors[0] + sensors[1] + sensors[2] + sensors[3] + sensors[4] + sensors[5]) < 500)
+      {
+        no_line = true;
+      }
     }
-    else if((sensors[0] + sensors[1] + sensors[2] + sensors[3] + sensors[4] + sensors[5]) < 500)
+
+    if (state == STATE_LEFT)
+    {
+      if (position > 2000) // Put your condition here
+      {
+        // Write your desired state here
+        next_state = STATE_FORWARD;
+      }
+      else if ((sensors[0] + sensors[1] + sensors[2] + sensors[3] + sensors[4] + sensors[5]) < 500)
+      {
+        no_line = true;
+      }
+    }
+
+    if (state == STATE_RIGHT)
+    {
+      if (position < 3000) // Put your condition here
+      {
+        // Write your desired state here
+        next_state = STATE_FORWARD;
+      }
+      else if ((sensors[0] + sensors[1] + sensors[2] + sensors[3] + sensors[4] + sensors[5]) < 500)
+      {
+        no_line = true;
+      }
+    }
+
+    if (no_line)
     {
       next_state = STATE_HALT;
     }
-    else if(position > 4000) // Put your condition here
-    {
-      // Write your desired state here
-      next_state = STATE_RIGHT;
-    }
     
-  }
+    state = next_state;
 
-  if(state == STATE_LEFT)
-  { 
-    if(position > 2000) // Put your condition here
+    switch (state)
     {
-      // Write your desired state here
-      next_state = STATE_FORWARD;
-    }
-    else if((sensors[0] + sensors[1] + sensors[2] + sensors[3] + sensors[4] + sensors[5]) < 500)
-    {
-      if void_count 
-      next_state = STATE_HALT;
-    }
-  }
-
-  if(state == STATE_RIGHT)
-  {
-    if(position < 3000) // Put your condition here
-    {
-      // Write your desired state here
-      next_state = STATE_FORWARD;
-    }
-    else if((sensors[0] + sensors[1] + sensors[2] + sensors[3] + sensors[4] + sensors[5]) < 500)
-    {
-      next_state = STATE_HALT;
-    }
-    
-  }
-
-  state = next_state;
-  
-  switch(state)
-  {
     case STATE_FORWARD:
       stateForward();
       break;
@@ -139,8 +146,7 @@ void selectState() {
 
     default:
       stateHalt();
+    }
   }
 }
-
-
 #include "zumo_driver.h"
